a801d603694631cf1142c8119d472dd5b2274c53
Alexis Reina <alexis@coldbridgestudio.com>
2019-02-05 10:47:32 +0100

    abstract sort and link funcions and use it in other collections than portfolio.

--- components/Partners.vue
+++ components/Partners.vue
@@ -1,87 +1,33 @@
 <template lang="html">
   <b-row class="partners">
-    <!-- <pre class="text-white">{{$cms}}</pre> -->
     <b-col
-      v-for="partner in partners"
+      v-for="partner in $cms.partners"
       :key="partner.id"
     >
       <b-img
         :src="partner.logo && require(`~/assets/${partner.logo.slice(1)}`)"
         :alt="partner.label"
         :title="partner.label"
-        :blank="!partner.logo"
-        :blank-color="!partner.logo ? '#fff' : null"
-        class="d-block mx-auto"
-        width="120"
-        height="60"
-        fluid
+        class="d-block mx-auto partners__logo"
       />
-
-      <!-- :class="{
-        'ml-md-auto mr-md-0': partners.indexOf(partner) === 0,
-        'ml-md-0 mr-md-auto': partners.indexOf(partner) === partners.length - 1
-      }" -->
-
-      <!--
-      <b-img-lazy
-        :src="partner.img.src"
-        :srcset="partner.img.srcset"
-        :alt="partner.img.label"
-      />
-      -->
-
     </b-col>
   </b-row>
 </template>
 
 <script>
-/* eslint-disable */
 export default {
-  name: "Partners",
-  computed: {
-    partners() {
-      const _partners = this.$cms.partners;
-      // const result = [];
-      const keys = Object.keys(_partners);
-      const list = keys.map(key => _partners[key]);
-      // const ids = keys.map(key => _partners[key].id).sort();
-
-      return keys
-        .map(key => _partners[key].id)
-        .sort()
-        .map(id => list.find(item => item.id === id));
-
-      // console.log(_result);
-      //
-      // ids.forEach(id => {
-      //   for (const key in _partners) {
-      //     const partner = _partners[key];
-      //
-      //     // console.log(partner);
-      //
-      //     if (partner.id === id) {
-      //       result.push(partner);
-      //     }
-      //   }
-      // });
-      //
-      // // console.log(result);
-      //
-      // return result;
-    }
-  }
-
-  // props: {
-  //   partners: {
-  //     type: Object,
-  //     required: true
-  //   }
-  // }
+  name: "Partners"
 };
 </script>
 
 <style lang="scss" scoped>
 .partners {
   background-color: $black;
+
+  &__logo {
+    max-height: 60px;
+    max-width: 100%;
+    width: auto;
+  }
 }
 </style>
--- plugins/my-netlify-cms-plugin.js
+++ plugins/my-netlify-cms-plugin.js
@@ -1,46 +1,71 @@
 import cms from "@/static/cms.json";
-// sort portfolio by entry time using v1 uuids
-// and link portolio items like in a linked list
-const portfolio = Object.keys(cms.portfolio)
-  // https://github.com/kelektiv/node-uuid/issues/75
-  .sort((a, b) => {
+
+// sort entries by creattion time using v1 uuids
+const sortCollection = ({ cms, key, reverse }) => {
+  const sortByUuidReverse = (a, b) => {
     a = a.replace(/^(.{8})-(.{4})-(.{4})/, "$3-$2-$1");
     b = b.replace(/^(.{8})-(.{4})-(.{4})/, "$3-$2-$1");
     return a > b ? -1 : a < b ? 1 : 0;
-  })
-  .reduce((all, uuid, idx, arr) => {
+  };
+
+  const sortByUuid = (a, b) => {
+    a = a.replace(/^(.{8})-(.{4})-(.{4})/, "$3-$2-$1");
+    b = b.replace(/^(.{8})-(.{4})-(.{4})/, "$3-$2-$1");
+    return a < b ? -1 : a > b ? 1 : 0;
+  };
+
+  // sort the keys and generate a new object
+  const sorted = Object.keys(cms[key])
+    .sort(reverse ? sortByUuidReverse : sortByUuid)
+    .reduce((all, uuid) => {
+      all[uuid] = cms[key][uuid];
+      return all;
+    }, {});
+
+  return sorted;
+};
+
+// link entries like in a linked list
+const linkCollection = ({ cms, key }) => {
+  const linked = Object.keys(cms[key]).reduce((all, uuid, idx, arr) => {
     const first = 0;
     const last = arr.length - 1;
 
-    all[uuid] = cms.portfolio[uuid];
+    all[uuid] = cms[key][uuid];
 
     if (idx >= first) {
       const prev = arr[idx + 1];
-      all[uuid].prev = cms.portfolio[prev];
+      all[uuid].prev = cms[key][prev];
     }
 
     if (idx <= last) {
       const next = arr[idx - 1];
-      all[uuid].next = cms.portfolio[next];
+      all[uuid].next = cms[key][next];
     }
-
-    // Link the first and last items
-    // to create a circular list
-
-    // if (idx === first) {
-    //   const next = arr[last];
-    //   all[uuid].next = cms.portfolio[next];
-    // }
-    //
-    // if (idx === arr.length - 1) {
-    //   const prev = arr[first];
-    //   all[uuid].prev = cms.portfolio[prev];
-    // }
 
     return all;
   }, {});
 
-cms.portfolio = portfolio;
+  return linked;
+};
+
+// this are my collections
+const collections = [
+  { key: "portfolio", reverse: true, link: true },
+  { key: "partners", reverse: false, link: false },
+  { key: "reviews", reverse: false, link: false }
+];
+
+// sort and link my collections
+collections.forEach(({ key, reverse, link }) => {
+  // replace collection with the sorted version
+  cms[key] = sortCollection({ cms, key, reverse });
+
+  // replace collection with the linked version
+  if (link) {
+    cms[key] = linkCollection({ cms, key });
+  }
+});
 
 export default ({ app }, inject) => {
   inject("cms", cms);
