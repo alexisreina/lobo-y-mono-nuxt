34090b029e90759f686d0065d8521b7a48fbe258
Alexis Reina <alexis@coldbridgestudio.com>
2019-02-20 01:18:16 +0100

    remove unused bootstrap-vue components

--- plugins/bootstrap-vue.js
+++ plugins/bootstrap-vue.js
@@ -1,20 +1,5 @@
 import Vue from "vue";
-import {
-  Button,
-  Form,
-  FormGroup,
-  FormInput,
-  FormTextarea,
-  Image,
-  Link,
-  Navbar
-} from "bootstrap-vue/es/components";
+import { Button, Image } from "bootstrap-vue/es/components";
 
 Vue.use(Button);
-Vue.use(Form);
-Vue.use(FormGroup);
-Vue.use(FormInput);
-Vue.use(FormTextarea);
 Vue.use(Image);
-Vue.use(Link);
-Vue.use(Navbar);
