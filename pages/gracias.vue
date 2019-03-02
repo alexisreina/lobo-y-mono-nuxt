39f050d2d13e4b5f7c0700dec3644caa0f50888b
Alexis Reina <alexis@coldbridgestudio.com>
2019-03-02 10:48:25 +0100

    improve thanks page

--- pages/gracias.vue
+++ pages/gracias.vue
@@ -1,23 +1,22 @@
 <template lang="html">
-  <div class="page-thanks py-5">
+  <div class="page-thanks py-3">
     <main role="main">
-      <section class="container py-5">
-        <div class="box py-5">
+      <section class="container">
+        <div class="box mx-auto">
           <img
             src="~/assets/svg/lym-thanks-lobo.svg"
             alt="Gracias"
-            class="img-fluid d-block mx-auto mb-5"
+            class="d-block mx-auto mb-5"
+            style="max-width:330px;width:100%"
           >
 
-          <h1 class="message mb-3">
+          <h1 class="h3 mb-3">
             ¡Gracias!<br>Pronto nos pondremos en contacto contigo
           </h1>
 
-          <div class="mb-3">
-            <nuxt-link :to="{ name: 'index' }" class="link">
-              Vuelve al inicio!
-            </nuxt-link>
-          </div>
+          <nuxt-link :to="{ name: 'index' }" class="link d-inline-block mb-5">
+            Vuelve al inicio!
+          </nuxt-link>
         </div>
       </section>
     </main>
@@ -35,13 +34,17 @@ export default {
 .page-thanks {
   min-height: 100vh;
   background-color: #fff;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 .box {
-  background-color: $gray-100;
   border-radius: 64px;
+  max-width: 450px;
   text-align: center;
 }
 .link {
+  display: inline-block;
   font-weight: bold;
   text-decoration: underline;
 }
